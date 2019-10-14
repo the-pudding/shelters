474d112d8fe2e63653ee8960020060998eba1b23
Amber Thomas <ProQuestionAsker@gmail.com>
2019-10-14 09:47:12 -0700

    fixing tilemap tooltip

--- src/js/pudding-chart/exports-template.js
+++ src/js/pudding-chart/exports-template.js
@@ -6,187 +6,190 @@
  4a. const charts = d3.selectAll('.thing').data(data).puddingChartLine();
  4b. const chart = d3.select('.thing').datum(datum).puddingChartLine();
 */
-import load from './../load-data'
+import load from "../load-data";
 
 d3.selection.prototype.exportsByState = function init(options) {
-	function createChart(el) {
-		const $container = d3.select('.exported')
-		const $sel = d3.select(el);
-		let data = $sel.datum();
-		// dimension stuff
-		let width = 0;
-		const height = 0;
-		const marginTop = 0;
-		const marginBottom = 0;
-		const marginLeft = 0;
-		const marginRight = 0;
+  function createChart(el) {
+    const $container = d3.select('.exported');
+    const $sel = d3.select(el);
+    let data = $sel.datum();
+    // dimension stuff
+    let width = 0;
+    const height = 0;
+    const marginTop = 0;
+    const marginBottom = 0;
+    const marginLeft = 0;
+    const marginRight = 0;
 
-		// scales
-		const scaleX = null;
-		const scaleY = null;
+    // scales
+    const scaleX = null;
+    const scaleY = null;
 
-		// dom elements
-		const $svg = null;
-		const $axis = null;
-		const $vis = null;
-		const $containerMini = null
-		const $tooltip = $container.select('.exported-tooltip')
-		let $stateTitle = null
-		let allDogs = null
-		let crosswalk = null
+    // dom elements
+    const $svg = null;
+    const $axis = null;
+    const $vis = null;
+    const $containerMini = null;
+    const $tooltip = $container.select('.exported-tooltip');
+    let $stateTitle = null;
+    let allDogs = null;
+    let crosswalk = null;
 
-		function handleMouseover(){
-			// select hovered animal, its data, and the parent state/country div
-			const hovered = d3.select(this)
-			const hoveredData = hovered.data()[0]
-			const parent = hovered.node().parentNode
-			const $selParent = d3.select(parent)
+    function handleMouseover() {
+      // select hovered animal, its data, and the parent state/country div
+      const hovered = d3.select(this);
+      const hoveredData = hovered.data()[0];
+      const parent = hovered.node().parentNode;
+      const $selParent = d3.select(parent);
 
-			// dim everyone except hovered dog
-			const dogs = $selParent.selectAll('.dog')
-				.classed('dimmed', true)
+      // dim everyone except hovered dog
+      const dogs = $selParent.selectAll('.dog').classed('dimmed', true);
 
-			hovered.classed('dimmed', false)
+      hovered.classed('dimmed', false);
 
-			// update tooltip info based on hovered dog
-			$tooltip.select('.tooltip-name').text(hoveredData.name)
-			$tooltip.select('.tooltip-desc').text(`${hoveredData.age} • ${hoveredData.sex}`)
-			$tooltip.select('.tooltip-breed').text(hoveredData.breed_secondary ? `${hoveredData.breed_primary} / ${hoveredData.breed_secondary} mix` : `${hoveredData.breed_primary}`)
+      // update tooltip info based on hovered dog
+      $tooltip.select('.tooltip-name').text(hoveredData.name);
+      $tooltip
+        .select('.tooltip-desc')
+        .text(`${hoveredData.age} • ${hoveredData.sex}`);
+      $tooltip
+        .select('.tooltip-breed')
+        .text(
+          hoveredData.breed_secondary
+            ? `${hoveredData.breed_primary} / ${hoveredData.breed_secondary} mix`
+            : `${hoveredData.breed_primary}`
+        );
 
-			// update tooltip location based on mouse location
-			const mouseX = d3.event.pageX
-			// const mouseY = d3.event.pageY
-			// const toolTipHeight = $tooltip.node().offsetHeight;
-			const toolTipWidth = $tooltip.node().offsetWidth;
+      // update tooltip location based on mouse location
+      const mouseX = d3.event.pageX;
+      // const mouseY = d3.event.pageY
+      // const toolTipHeight = $tooltip.node().offsetHeight;
+      const toolTipWidth = $tooltip.node().offsetWidth;
 
-			$tooltip
-				.style('left', () => {
-					let xMove = mouseX
-					if (mouseX > 0.5 * width) xMove -= toolTipWidth
-					console.log({xMove, width})
-					return `${xMove}px`
-				})
-				.style('top', `${d3.event.pageY}px`)
-				.classed('is-hidden', false)
+      $tooltip
+        .style('left', () => {
+          let xMove = mouseX;
+          if (mouseX > 0.5 * width) xMove -= toolTipWidth;
+          console.log({ xMove, width });
+          return `${xMove}px`;
+        })
+        .style('top', `${d3.event.pageY}px`)
+        .classed('is-hidden', false);
+    }
 
+    function handleMouseout() {
+      const section = d3.select(this);
+      d3.selectAll('.dog').classed('dimmed', false);
 
+      $tooltip.classed('is-hidden', true);
+    }
 
-		}
+    function importCrosswalk() {
+      load
+        .loadCrosswalk()
+        .then(result => {
+          crosswalk = d3.map(result[0], d => d.file);
+        })
+        .catch(console.error);
+    }
 
-		function handleMouseout(){
-			const section = d3.select(this)
-			d3.selectAll('.dog')
-				.classed('dimmed', false)
+    const Chart = {
+      // called once at start
+      init() {
+        importCrosswalk();
+        Chart.resize();
+        Chart.render();
+      },
+      // on resize, update new dimensions
+      resize() {
+        // defaults to grabbing dimensions from container element
+        width = $sel.node().offsetWidth - marginLeft - marginRight;
+        // height = $sel.node().offsetHeight - marginTop - marginBottom;
+        // $svg
+        // 	.attr('width', width + marginLeft + marginRight)
+        // 	.attr('height', height + marginTop + marginBottom);
+        return Chart;
+      },
+      // update scales and render chart
+      render() {
+        const $state = $sel
+          .selectAll('.state')
+          .data(data, d => `${d.key}-${d.values.length}`)
+          .join(enter => {
+            const state = enter.append('div').attr('class', 'state');
 
-			$tooltip.classed('is-hidden', true)
-		}
+            $stateTitle = state.append('p').attr('class', 'state-name');
 
-		function importCrosswalk(){
-			load.loadCrosswalk()
-				.then(result => {
-					crosswalk = d3.map(result[0], d => d.file)
-				})
-				.catch(console.error)
+            const $container = state
+              .append('div')
+              .attr('class', 'container-mini');
 
-		}
+            return state;
+            // .enter()
+            // .append('div')
+            // .attr('class', 'dog')
+            //
+          });
 
-		const Chart = {
-			// called once at start
-			init() {
+        $stateTitle.text(d => `${d.key} • ${d.values.length}`);
 
-				importCrosswalk()
-				Chart.resize();
-				Chart.render();
-			},
-			// on resize, update new dimensions
-			resize() {
-				// defaults to grabbing dimensions from container element
-				width = $sel.node().offsetWidth - marginLeft - marginRight;
-				// height = $sel.node().offsetHeight - marginTop - marginBottom;
-				// $svg
-				// 	.attr('width', width + marginLeft + marginRight)
-				// 	.attr('height', height + marginTop + marginBottom);
-				return Chart;
-			},
-			// update scales and render chart
-			render() {
-				const $state = $sel.selectAll('.state')
-					.data(data, d => `${d.key}-${d.values.length}`)
-					.join(
-						enter => {
-							const state = enter
-								.append('div')
-								.attr('class', 'state')
+        $state
+          .select('.container-mini')
+          .selectAll('.dog')
+          .data(d => d.values.sort((a, b) => d3.ascending(a.file, b.file)))
+          .join(enter => {
+            allDogs = enter
+              .append('div')
+              .attr('class', 'dog')
+              .classed('highlighted', d => d.highlighted)
+              .on('mouseover', handleMouseover)
+              .on('mouseout', handleMouseout);
 
-							$stateTitle = state.append('p')
-								.attr('class', 'state-name')
+            return allDogs;
+          })
+          .style('background-image', d =>
+            d.file === 'mix'
+              ? 'url(assets/images/profiles/labrador.png)'
+              : `url(assets/images/profiles/${d.file}.png)`
+          );
 
-							const $container = state.append('div')
-								.attr('class', 'container-mini')
+        //
+        // const sorted = data.values
+        // 	.sort((a, b) => d3.ascending(a.size, b.size))
+        // 	.sort((a,b) => {
+        // 		return d3.ascending(a.file, b.file)
+        // 	})
 
-							return state
-							// .enter()
-							// .append('div')
-							// .attr('class', 'dog')
-							//
-						}
-					);
+        // $containerMini.selectAll('.dog')
+        // 	.data(sorted)
+        // 	.join(
+        // 		enter => {
+        // 			enter.append('div')
+        // 				.attr('class', 'dog')
+        // 				.style('background-image', d => `url(assets/images/profiles/${d.file}.png)`)
+        // 		},
+        // 		exit => {
+        // 			exit.remove()
+        // 		}
+        // 	)
 
-				$stateTitle.text(d => `${d.key} • ${d.values.length}`)
+        return Chart;
+      },
+      // get / set data
+      data(val) {
+        if (!arguments.length) return data;
+        data = val;
+        $sel.datum(data);
+        Chart.render();
+        return Chart;
+      },
+    };
+    Chart.init();
 
-				$state.select('.container-mini').selectAll('.dog')
-					.data(d => d.values.sort((a, b) => d3.ascending(a.file, b.file)))
-					.join(enter => {
-						allDogs = enter.append('div')
-							.attr('class', 'dog')
-							.classed('highlighted', d => d.highlighted)
-							.on('mouseover', handleMouseover)
-							.on('mouseout', handleMouseout)
+    return Chart;
+  }
 
-						return allDogs
-					})
-					.style('background-image', d => d.file === 'mix' ? 'url(assets/images/profiles/labrador.png)' : `url(assets/images/profiles/${d.file}.png)`)
-
-
-				//
-				// const sorted = data.values
-				// 	.sort((a, b) => d3.ascending(a.size, b.size))
-				// 	.sort((a,b) => {
-				// 		return d3.ascending(a.file, b.file)
-				// 	})
-
-				// $containerMini.selectAll('.dog')
-				// 	.data(sorted)
-				// 	.join(
-				// 		enter => {
-				// 			enter.append('div')
-				// 				.attr('class', 'dog')
-				// 				.style('background-image', d => `url(assets/images/profiles/${d.file}.png)`)
-				// 		},
-				// 		exit => {
-				// 			exit.remove()
-				// 		}
-				// 	)
-
-
-
-				return Chart;
-			},
-			// get / set data
-			data(val) {
-				if (!arguments.length) return data;
-				data = val;
-				$sel.datum(data);
-				Chart.render();
-				return Chart;
-			}
-		};
-		Chart.init();
-
-		return Chart;
-	}
-
-	// create charts
-	const charts = this.nodes().map(createChart);
-	return charts.length > 1 ? charts : charts.pop();
+  // create charts
+  const charts = this.nodes().map(createChart);
+  return charts.length > 1 ? charts : charts.pop();
 };
--- src/js/tile-movement.js
+++ src/js/tile-movement.js
@@ -1,67 +1,64 @@
-import load from './load-data'
-import './pudding-chart/tile-template'
-import lookup from './utils/lookup-state-abbr'
+import load from './load-data';
+import './pudding-chart/tile-template';
+import lookup from './utils/lookup-state-abbr';
 
 // data
-let importExport = null
-let tileLoc = null
+let importExport = null;
+let tileLoc = null;
 
 // selections
-const $section = d3.select('.movement')
-const $container = $section.select('.figure-container')
-const $tooltip = $section.select('.movement-tooltip')
+const $section = d3.select('.movement');
+const $container = $section.select('.figure-container');
+const $tooltip = $section.select('.movement-tooltip');
 
 // charts
-let charts = null
-let width = null
+let charts = null;
+let width = null;
 
-function handleMouseover(){
-	const sel = d3.select(this)
-	const state = sel.attr('data-state')
-	// $tooltip
-	// 	.style('left', `${d3.event.pageX}px`)
-	// 	.style('top', `${d3.event.pageY}px`)
-	// 	.classed('is-hidden', false)
+function handleMouseover() {
+  const sel = d3.select(this);
+  const state = sel.attr('data-state');
 
-	if (state !== 'blank'){
-		const imported = sel.attr('data-imported')
-		const exported = sel.attr('data-exported')
-		$tooltip.select('.tooltip-state').text(sel.attr('data-state'))
-		$tooltip.select('.tooltip-count-import').text(imported)
-		$tooltip.select('.tooltip-count-export').text(exported)
+  if (state !== 'blank') {
+    const imported = sel.attr('data-imported');
+    const exported = sel.attr('data-exported');
+    $tooltip.select('.tooltip-state').text(sel.attr('data-state'));
+    $tooltip.select('.tooltip-count-import').text(imported);
+    $tooltip.select('.tooltip-count-export').text(exported);
 
-		$tooltip.classed('is-hidden', false)
-	}
-	else $tooltip.classed('is-hidden', true)
+    $tooltip.classed('is-hidden', false);
+  } else $tooltip.classed('is-hidden', true);
 
-  const mouseX = d3.event.pageX
-  // const mouseY = d3.event.pageY
-  // const toolTipHeight = $tooltip.node().offsetHeight;
+  const mouseX = d3.event.pageX;
   const toolTipWidth = $tooltip.node().offsetWidth;
 
   $tooltip
     .style('left', () => {
-      let xMove = mouseX
-      if (mouseX > 0.5 * width) xMove -= toolTipWidth
-      return `${xMove}px`
+      let xMove = mouseX;
+      if (mouseX > 0.5 * width) xMove -= toolTipWidth;
+      return `${xMove}px`;
     })
     .style('top', `${d3.event.pageY}px`)
-    .classed('is-hidden', false)
+    .classed('is-hidden', false);
 }
 
-function cleanData(arr){
-	return arr.map((d, i) => {
-		return {
-			...d,
+function handleMouseout() {
+  $tooltip.classed('is-hidden', true);
+}
+
+function cleanData(arr) {
+  return arr.map((d, i) => {
+    return {
+      ...d,
       imported: +d.imported,
       exported: +d.exported,
-      number: i
-		}
-	})
+      number: i,
+    };
+  });
 }
 
-function setup(){
-  const locationMap = d3.map(importExport, d => d.location)
+function setup() {
+  const locationMap = d3.map(importExport, d => d.location);
 
   // add data to tile location
   const tileData = tileLoc.map((d, i) => {
@@ -69,47 +66,50 @@ function setup(){
       location: d,
       gridNumber: i + 1,
       count: locationMap.get(d),
-      abbreviation: d === 'blank' ? null : lookup(d)
-    }
-  })
+      abbreviation: d === 'blank' ? null : lookup(d),
+    };
+  });
 
   charts = $container
-		.selectAll('.grid-block')
-		.data(tileData)
-    .join(enter => enter
-			.append('div')
-			.attr('class', d => `grid-block block-${d.location}`)
-			.attr('data-state', d => d.location)
-			.attr('data-imported', d => (d.count) ? d.count.imported : null)
-			.attr('data-exported', d => (d.count) ? d.count.exported : null)
-			.on('mouseover', handleMouseover)
-		)
-		.tileMap()
-
+    .selectAll('.grid-block')
+    .data(tileData)
+    .join(enter =>
+      enter
+        .append('div')
+        .attr('class', d => `grid-block block-${d.location}`)
+        .attr('data-state', d => d.location)
+        .attr('data-imported', d => (d.count ? d.count.imported : null))
+        .attr('data-exported', d => (d.count ? d.count.exported : null))
+        .on('mouseover', handleMouseover)
+        .on('mouseout', handleMouseout)
+    )
+    .tileMap();
 }
 
-function loadLocations(){
-  load.loadJSON('tile-locations.json')
+function loadLocations() {
+  load
+    .loadJSON('tile-locations.json')
     .then(result => {
-      tileLoc = result
-      setup()
+      tileLoc = result;
+      setup();
     })
-    .catch(console.error)
+    .catch(console.error);
 }
 
-function resize(){
-  width = $section.node().offsetWidth
-	if (charts) charts.forEach(d => d.resize())
+function resize() {
+  width = $section.node().offsetWidth;
+  if (charts) charts.forEach(d => d.resize());
 }
 
-function init(){
-  load.loadCSV('importExport.csv')
+function init() {
+  load
+    .loadCSV('importExport.csv')
     .then(result => {
-      importExport = cleanData(result)
-      loadLocations()
-      resize()
+      importExport = cleanData(result);
+      loadLocations();
+      resize();
     })
-    .catch(console.error)
+    .catch(console.error);
 }
 
-export default {init, resize}
+export default { init, resize };
